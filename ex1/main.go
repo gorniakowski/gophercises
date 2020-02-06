cd1207a57c3a06c8b17e6728f24bdf0bf0f23a2c
gorniak <gorniakowski@gmail.com>
2020-02-06 11:36:48 +0100

    end of part 2 ex1

--- ex1/main.go
+++ ex1/main.go
@@ -7,11 +7,13 @@ import (
 	"io"
 	"log"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
-
 	filename := flag.String("csv", "problems.csv", "a csv file")
+	seconds := flag.Int("t", 30, "number of seconds for timer ")
 	flag.Parse()
 	file, err := os.Open(*filename)
 	if err != nil {
@@ -22,6 +24,14 @@ func main() {
 	r := csv.NewReader(file)
 	counter, score := 0, 0
 	var answer string
+	fmt.Printf("You have got %d seconds. Press Enter to start", *seconds)
+	fmt.Scanln()
+	timer := time.NewTimer(time.Duration(*seconds) * time.Second)
+	go func() {
+		<-timer.C
+		fmt.Printf("\n Time up! Your score is %d out of %d\n", score, counter)
+		os.Exit(0)
+	}()
 	for {
 		counter++
 		record, err := r.Read()
@@ -37,10 +47,10 @@ func main() {
 			fmt.Println(err)
 			break
 		}
-		if answer == record[1] {
+		if answer == strings.TrimSpace(record[1]) {
 			score++
 		}
 	}
-	fmt.Printf("Your score is %d out of %d", score, counter)
+	fmt.Printf("Your score is %d out of %d\n", score, counter)
 
 }
