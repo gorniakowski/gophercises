0bb14f049ed7868f1b1806be711b39fdbf84775c
gorniak <gorniakowski@gmail.com>
2020-02-14 09:28:16 +0100

    more refinements

--- ex3/main.go
+++ ex3/main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gophercises/ex3/parser"
 	"html/template"
+	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
-const fileName = "gopher.json"
-
 func main() {
-	arcs, err := parser.ParseJson(fileName)
+	fileName := flag.String("file", "gopher.json", "you give file with story")
+	flag.Parse()
+	arcs, err := parser.ParseJson(*fileName)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -23,14 +26,21 @@ func main() {
 
 func arcHandler(arcs map[string]parser.Arc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		var arc parser.Arc
-		if r.URL.Path[1:] == "" {
-			arc = arcs["intro"]
-		} else {
-			arc = arcs[r.URL.Path[1:]]
-		}
 		t, _ := template.ParseFiles("template.html")
-		t.Execute(w, arc)
+		path := strings.TrimSpace(r.URL.Path)
+		if path == "" || path == "/" {
+			path = "/intro"
+		}
+
+		if arc, ok := arcs[path[1:]]; ok {
+			err := t.Execute(w, arc)
+			if err != nil {
+				log.Printf("%v", err)
+				http.Error(w, "something went wrong", http.StatusInternalServerError)
+			}
+			return
+		}
+		http.Error(w, "Chapter not found", http.StatusNotFound)
 
 	}
 }
