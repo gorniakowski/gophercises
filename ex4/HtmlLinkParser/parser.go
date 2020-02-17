407e92d4de7abd28ccfd536948b11745ec391226
gorniak <gorniakowski@gmail.com>
2020-02-17 16:05:56 +0100

    rewrite for better recursion

--- ex4/HtmlLinkParser/parser.go
+++ ex4/HtmlLinkParser/parser.go
@@ -2,6 +2,7 @@ package HtmlLinkParser
 
 import (
 	"io"
+	"strings"
 
 	"golang.org/x/net/html"
 )
@@ -14,13 +15,53 @@ type Link struct {
 }
 
 func ParseHtml(input io.Reader) (DocumentLinks, error) {
-	nodes, err := html.Parse(input)
+	node, err := html.Parse(input)
 	if err != nil {
 		return nil, err
 	}
-	findLinks(nodes)
-	return nil, nil
+	linkNodes := findLinkNodes(node)
+	var links DocumentLinks
+	for _, node := range linkNodes {
+		links = append(links, buildLink(node))
+	}
+
+	return links, nil
 }
-func findLinks(nodes *html.Node) []Link {
+func findLinkNodes(node *html.Node) []*html.Node {
+	if node.Type == html.ElementNode && node.Data == "a" {
+		return []*html.Node{node}
+	}
+	var result []*html.Node
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		result = append(result, findLinkNodes(c)...)
+	}
+
+	return result
+}
+
+func buildLink(node *html.Node) Link {
+	var result Link
+	for _, attr := range node.Attr {
+		if attr.Key == "href" {
+			result.Href = attr.Val
+			break
+		}
+	}
+	result.Text = findText(node)
+	return result
+}
+
+func findText(node *html.Node) string {
+	if node.Type == html.TextNode {
+		return node.Data
+	}
+	if node.Type != html.ElementNode {
+		return ""
+	}
+	var result string
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		result += findText(c)
+	}
+	return strings.Join(strings.Fields(result), " ")
 
 }
--- ex4/main.go
+++ ex4/main.go
@@ -2,20 +2,24 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"gophercises/ex4/HtmlLinkParser"
 	"os"
 )
 
 func main() {
+	var result []HtmlLinkParser.Link
 	fileName := flag.String("f", "ex1.html", "html file to parse")
 	flag.Parse()
 	file, err := os.Open(*fileName)
 	if err != nil {
 		panic(err)
 	}
-	_, err = HtmlLinkParser.ParseHtml(file)
+	result, err = HtmlLinkParser.ParseHtml(file)
 	if err != nil {
 		panic(err)
 	}
-
+	for _, link := range result {
+		fmt.Printf("%+v", link)
+	}
 }
